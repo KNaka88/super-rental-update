ae5dffca783a235012761046521f48f3511d5f77
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-04-02 18:18:07 -0700

    change the update paths to inside rental-tile.hbs

--- app/components/rental-detail.js
+++ app/components/rental-detail.js
@@ -9,6 +9,23 @@ export default Ember.Component.extend({
     },
     destroyReview(review) {
       this.sendAction('destroyReview', review);
+    },
+
+    updateRentalForm() {
+      this.set('updateRentalForm', true);
+    },
+    update(rental) {
+      var params = {
+        owner: this.get('owner'),
+        city: this.get('city'),
+        type: this.get('type'),
+        image: this.get('image'),
+        bedrooms: this.get('bedrooms'),
+      };
+      this.set('updateRentalForm', false);
+      this.sendAction('update', rental, params);
     }
+
+
   }
 });
